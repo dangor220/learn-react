ae40046b5c47a8bc74ab35b34da1cc68dffec095
Danil Gordeev <dan.gor220@yandex.ru>
2023-02-18 00:53:55 +0300

    feat: learning state and lifecycle

--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+class Clock extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			date: new Date(),
+		}
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(() => this.tick(), 1000)
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID)
+	}
+
+	tick() {
+		this.setState({ date: new Date()})
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Clock</h1>
+				<p>{this.state.date.toLocaleTimeString()}</p>
+			</div>
+		);
+	}
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Clock/>);
+
 // class App extends React.Component {
 //   render() {
 //     return (
@@ -19,45 +52,44 @@ import ReactDOM from 'react-dom/client';
 //   }
 // }
 
-function Comment(props) {
-	console.log(props);
-	return (
-		<div className="Comment">
-			<UserInfo user={props.author} />
-			<div className="Comment-text">{props.text}</div>
-			<div className="Comment-date">{formatDate(props.date)}</div>
-		</div>
-	);
-}
+// function Comment(props) {
+// 	return (
+// 		<div className="Comment">
+// 			<UserInfo user={props.author} />
+// 			<div className="Comment-text">{props.text}</div>
+// 			<div className="Comment-date">{formatDate(props.date)}</div>
+// 		</div>
+// 	);
+// }
 
-function Avatar(props) {
-	console.log(props);
-	return (
-		<img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
-	);
-}
+// function Avatar(props) {
+// 	console.log(props);
+// 	return (
+// 		<img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+// 	);
+// }
 
-function UserInfo(props) {
-	return (
-		<div className="UserInfo">
-			<Avatar user={props.user} />
-			<div className="UserInfo-name">{props.user.name}</div>
-		</div>
-	);
-}
+// function UserInfo(props) {
+// 	return (
+// 		<div className="UserInfo">
+// 			<Avatar user={props.user} />
+// 			<div className="UserInfo-name">{props.user.name}</div>
+// 		</div>
+// 	);
+// }
 
-function formatDate(date) {
-	return <h1>{date}</h1>;
-}
+// function formatDate(date) {
+// 	return <h1>{date}</h1>;
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<Comment
-		author={{
-			name: 'dangor',
-			avatarUrl: 'https://avatars.githubusercontent.com/u/50300277?v=4',
-		}}
-		text="complete lesson"
-		date="02.10.2022"
-	/>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+// 	<Comment
+// 		author={{
+// 			name: 'dangor',
+// 			avatarUrl: 'https://avatars.githubusercontent.com/u/50300277?v=4',
+// 		}}
+// 		text="complete lesson"
+// 		date="02.10.2022"
+// 	/>
+// );
