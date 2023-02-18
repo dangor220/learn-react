eca9b1c1df52ae67bb8319c8992cabed6ed6ab57
Danil Gordeev <dan.gor220@yandex.ru>
2023-02-18 18:29:38 +0300

    feat: add event in component

--- src/index.js
+++ src/index.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class Clock extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			date: new Date(),
-		}
-	}
+// class Clock extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			date: new Date(),
+// 		}
+// 	}
 
-	componentDidMount() {
-		this.timerID = setInterval(() => this.tick(), 1000)
-	}
+// 	componentDidMount() {
+// 		this.timerID = setInterval(() => this.tick(), 1000)
+// 	}
 
-	componentWillUnmount() {
-		clearInterval(this.timerID)
-	}
+// 	componentWillUnmount() {
+// 		clearInterval(this.timerID)
+// 	}
 
-	tick() {
-		this.setState({ date: new Date()})
-	}
+// 	tick() {
+// 		this.setState({ date: new Date()})
+// 	}
 
-	render() {
-		return (
-			<div>
-				<h1>Clock</h1>
-				<p>{this.state.date.toLocaleTimeString()}</p>
-			</div>
-		);
-	}
-}
+// 	render() {
+// 		return (
+// 			<div>
+// 				<h1>Clock</h1>
+// 				<p>{this.state.date.toLocaleTimeString()}</p>
+// 			</div>
+// 		);
+// 	}
+// }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Clock/>);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Clock/>);
 
 // class App extends React.Component {
 //   render() {
@@ -93,3 +93,32 @@ root.render(<Clock/>);
 // 		date="02.10.2022"
 // 	/>
 // );
+
+class Toggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isToggle: true,
+		}
+		this.handldeClick = this.handldeClick.bind(this)
+	}
+
+	handldeClick() {
+		this.setState((prevState) => ({
+			isToggle: !prevState.isToggle
+		}));
+	}	
+
+	render() {
+		return (
+			<button onClick={this.handldeClick}>
+				{this.state.isToggle ? "Включено" : "Выключено"}
+			</button>
+		)
+	}
+
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Toggle/>);
